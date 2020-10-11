a6238ac3bd04083d6a73ac9e86b17f1a3dca0edc
Melisa Budini <melixbq@gmail.com>
2020-10-11 18:03:42 -0300

    add: validates, dashboard

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -1,2 +1,5 @@
 class Proyecto < ApplicationRecord
+    validates :name, presence: true, allow_blank: false
+    validates :description, presence: true, allow_blank: false
+    validates :state, presence: true, allow_blank: false
 end
